be4b6e6100ae7afa5f258f2f8d2cb18dcecf08f9
geket <milkymilkshake72@gmail.com>
2025-06-13 05:35:52 -0400

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,2 +1,8 @@
+# iptables method
 sudo apt install iptables
 sudo iptables -t mangle -A POSTROUTING -j TTL --ttl-set 65
+sudo ip6tables -t mangle -A POSTROUTING -j HL --hl-set 65
+# Changing the default TTL with sysctl
+# Optional, may cause issues on non-hotspot networks
+# sudo sysctl -w net.ipv4.ip_default_ttl=65
+# sudo sysctl -w net.ipv6.ip_default_ttl=65
